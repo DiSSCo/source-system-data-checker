20ec0ebf18a558b048e1a2720a459a3a68c8c097
Soulaine <soulaine.theo@gmail.com>
2026-01-27 13:43:59 +0100

    Initial commit

---
+++ src/main/java/eu/dissco/sourcesystemdatachecker/SourceSystemDataCheckerApplication.java
@@ -0,0 +1,13 @@
+package eu.dissco.sourcesystemdatachecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourceSystemDataCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SourceSystemDataCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/dissco/sourcesystemdatachecker/SourceSystemDataCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.dissco.sourcesystemdatachecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SourceSystemDataCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
